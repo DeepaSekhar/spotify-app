53a98fab848f7de27cb9f42b6ab78d583b02e4cf
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-07-16 17:29:01 +0100

    more interface

---
+++ src/app/models/ID.ts
@@ -0,0 +1,5 @@
+export enum ID {
+    The1HY2Jd0NmPuamShAr6KMms = "1HY2Jd0NmPuamShAr6KMms",
+    The2LolQgalUvZDfp5VvVTTYV = "2lolQgalUvZDfp5vvVtTYV",
+    The4VIvfOurcf0VuLRxLkGnIG = "4VIvfOurcf0vuLRxLkGnIG",
+}
