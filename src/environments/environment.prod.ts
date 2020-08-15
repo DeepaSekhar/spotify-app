60e99360a192ee33ac561de423fd5506f9ffcf20
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-08-15 19:07:53 +0100

    Added the login link to localhost and production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  oAuthUrl: "https://accounts.spotify.com/authorize?client_id=912a79b35603492291b382ebd0733970&redirect_uri=https%3A%2F%2Fdeepasekhar.github.io%2Fspotify-app%2Fsearch%2F&scope=user-read-private%20user-read-email&response_type=token&state=123"
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,6 @@ export const environment = {
   production: false,
   clientId: '912a79b35603492291b382ebd0733970',
   clientSecret: '0338d98a37d9471ea017fd10c0275e8f',
-  redirectUri: 'http://localhost:4200/search'
+  redirectUri: 'http://localhost:4200/search',
+  oAuthUrl: "https://accounts.spotify.com/authorize?client_id=912a79b35603492291b382ebd0733970&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fsearch%2F&scope=user-read-private%20user-read-email&response_type=token&state=123"
 }
