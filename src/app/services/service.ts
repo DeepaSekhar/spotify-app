22cca0b5b15cfb5aeff3923f1ea3affb7f05d5d1
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-08-15 19:06:53 +0100

    change OAuthurl accoording to enviornment file

--- src/app/services/service.ts
+++ src/app/services/service.ts
@@ -26,8 +26,8 @@ export class SpotifyService {
     oAuthToken: string;
     //oAuth url for authorise and login 
     // readonly oAuthUrl = "https://accounts.spotify.com/authorize?client_id=912a79b35603492291b382ebd0733970&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fsearch%2F&scope=user-read-private%20user-read-email&response_type=token&state=123";
-    readonly oAuthUrl = "https://accounts.spotify.com/authorize?client_id=912a79b35603492291b382ebd0733970&redirect_uri=https%3A%2F%2Fdeepasekhar.github.io%2Fspotify-app%2Fsearch%2F&scope=user-read-private%20user-read-email&response_type=token&state=123";
-
+    // readonly oAuthUrl = "https://accounts.spotify.com/authorize?client_id=912a79b35603492291b382ebd0733970&redirect_uri=https%3A%2F%2Fdeepasekhar.github.io%2Fspotify-app%2Fsearch%2F&scope=user-read-private%20user-read-email&response_type=token&state=123";
+    readonly oAuthUrl = environment.oAuthUrl;
 
     constructor(private http: HttpClient) {
     }
